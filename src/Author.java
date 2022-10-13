d1a7f95f1d3a66a7d7e9750a39aef63ed9adfcac
Glebova <e.tulzakova@mail.ru>
2022-10-13 18:13:21 +0400

    Домашнее задание выполнено

--- src/Author.java
+++ src/Author.java
@@ -15,4 +15,28 @@ public class Author {
     public String getLastName() {
         return this.lastName;
     }
+    public String toString(){
+        return this.firstName + " " + this.lastName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj== null){
+            return false;
+        }
+        if (this==obj){
+            return true;
+        }
+        if (!(obj instanceof Author)){
+            return false;
+        }
+        Author that = (Author) obj;
+        return this.firstName.equals(that.firstName) && this.lastName.equals(that.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
+
